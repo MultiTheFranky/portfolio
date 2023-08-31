1d612c681131d8091790ee43e1baec02b51eca77
Francisco Alamo Garcia <fran.minecraft.mta@gmail.com>
2023-08-31 17:54:01 +0000

    feat: prettier

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,14 +1,12 @@
 module.exports = {
-  extends: [
-    "@commitlint/config-conventional",
-  ],
-  parserPreset: "conventional-changelog-conventionalcommits",
-  rules: {
-    "header-max-length": [2, "always", "100"],
-    "type-enum": [
-      2,
-      "always",
-      ["BREAKING", "chore", "ci", "feat", "fix", "refactor", "revert"],
-    ],
-  },
+    extends: ["@commitlint/config-conventional"],
+    parserPreset: "conventional-changelog-conventionalcommits",
+    rules: {
+        "header-max-length": [2, "always", "100"],
+        "type-enum": [
+            2,
+            "always",
+            ["BREAKING", "chore", "ci", "feat", "fix", "refactor", "revert"]
+        ]
+    }
 };
