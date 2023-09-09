e126f561e3f2fa2ce05e41ae49a80571f17fe35d
MultiTheFranky <fran.minecraft.mta@gmail.com>
2023-09-09 16:17:37 +0200

    fix: test and lint

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,5 +1,5 @@
-it('Incorrect titles', () => {
-  const page = cy.visit('http://localhost:4321');
-  page.get('title').should('have.text', 'MultiTheFranky')
-  page.get('h1').should('have.text', 'MultiTheFranky');
+it("Correct index structure", () => {
+    const page = cy.visit("http://localhost:4321");
+    page.get("title").should("have.text", "MultiTheFranky");
+    page.get("h1").should("have.text", "\nTimeline\n\nKnowledge\n");
 });
